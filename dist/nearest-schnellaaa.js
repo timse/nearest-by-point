09962403847d936878212a1d0e2d5e40e1cccbdb
Tim Sebastian <tim.sebastian@gmail.com>
2014-08-03 13:15:10 +0200

    add dist for non-viewport checking

--- dist/nearest-schnellaaa.js
+++ dist/nearest-schnellaaa.js
@@ -1,56 +1,16 @@
 /*!
 * nearest-schnellaaa - jQuery plugin to find filter a selection of elements to only those near a certain point
-* v0.0.3 - 2014-08-03 9:19:49 AM UTC
+* v0.0.3 - 2014-08-03 11:10:08 AM UTC
 * Copyright (c) 2014 timse; Licensed 
 */
  ;(function($){
-;var $win, ensureListeners, filterFn, noop, resizeListener, throttle, viewportHeight, viewportWidth;
+;var $win, filterFn;
 
 $win = $(window);
-
-throttle = function(fn, wait) {
-  var last, throttled, timeoutTkn;
-  last = null;
-  timeoutTkn = null;
-  return throttled = function() {
-    var diff, now;
-    now = +(new Date);
-    diff = now - last;
-    if (!last || diff > wait) {
-      last = now;
-      return fn();
-    } else {
-      clearTimeout(timeoutTkn);
-      return timeoutTkn = setTimeout(throttled, wait - diff + 1);
-    }
-  };
-};
-
-viewportHeight = viewportWidth = null;
-
-resizeListener = function() {
-  viewportWidth = $win.width();
-  viewportHeight = $win.height();
-};
-
-resizeListener();
-
-noop = function() {};
-
-ensureListeners = function() {
-  ensureListeners = noop;
-  $win.on('resize', throttle(resizeListener, 50));
-};
 
 filterFn = function(left, right, top, bottom, elem) {
   var boundary;
   boundary = elem.getBoundingClientRect();
-  if (boundary.bottom < 0) {
-    return false;
-  }
-  if (boundary.top > viewportHeight) {
-    return false;
-  }
   if (boundary.right < left) {
     return false;
   }
@@ -68,7 +28,6 @@ filterFn = function(left, right, top, bottom, elem) {
 
 $.fn['nearest-schnellaaa'] = $.fn['nearestSchnellaaa'] = function(x, y, prox) {
   var bottom, elem, left, res, right, top, _i, _len;
-  ensureListeners();
   left = x - prox;
   top = y - prox;
   right = x + prox;
